c85f26d3a254cd2d57860c03f5003b767928db57
Romaing <rom.cremery@gmail.com>
2016-05-24 20:15:55 +0200

    Implémentation fond

--- Runner/animatedgraphicelement.cpp
+++ Runner/animatedgraphicelement.cpp
@@ -1,5 +1,34 @@
 #include "animatedgraphicelement.h"
+#include <SFML/Graphics.hpp>
 
-AnimatedGraphicElement::AnimatedGraphicElement()
+AnimatedGraphicElement::AnimatedGraphicElement(const std::vector<sf::IntRect>& clipRects, sf::Texture &image, int x, int y, int w, int h)
+    :GraphicElement(image, x, y, w, h)
 {
+    m_clipRects = clipRects;
+    m_clipRect = 0;
+    m_nb_etapes = 8;
+    m_etape = 0;
+    setTextureRect(m_clipRects[m_clipRect]);
+}
+
+AnimatedGraphicElement::AnimatedGraphicElement(const AnimatedGraphicElement &copie)
+    :GraphicElement(copie)
+{
+    m_clipRects = copie.m_clipRects;
+    m_clipRect = copie.m_clipRect;
+    m_nb_etapes = copie.m_nb_etapes;
+    m_etape = copie.m_etape;
+    setTextureRect(m_clipRects[m_clipRect]);
+}
+
+void AnimatedGraphicElement::draw(sf::RenderWindow *window)
+{
+    m_etape++;
+    if(m_etape>m_nb_etapes)
+    {
+        m_etape = 0;
+        m_clipRect = m_clipRect % m_clipRects.size()+1;
+        setTextureRect(m_clipRects[m_clipRect-1]);
+    }
+    window->draw(*this);
 }
--- Runner/animatedgraphicelement.h
+++ Runner/animatedgraphicelement.h
@@ -1,10 +1,20 @@
 #ifndef ANIMATEDGRAPHICELEMENT_H
 #define ANIMATEDGRAPHICELEMENT_H
 
-class AnimatedGraphicElement
+#include <GraphicElement.h>
+#include <SFML/Graphics.hpp>
+
+class AnimatedGraphicElement : public GraphicElement
 {
+private:
+    std::vector<sf::IntRect> m_clipRects;
+    int m_nb_etapes;
+    int m_etape;
+    int m_clipRect;
 public:
-    AnimatedGraphicElement();
+    AnimatedGraphicElement(const std::vector<sf::IntRect>& clipRects, sf::Texture &image, int x, int y, int w, int h);
+    AnimatedGraphicElement(const AnimatedGraphicElement& copy);
+    void draw(sf::RenderWindow *window);
 };
 
 #endif // ANIMATEDGRAPHICELEMENT_H
